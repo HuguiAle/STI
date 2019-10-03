67c041a449a435da74b52fc42df3ba35ea3b69e6
HuguiAle <56133191+HuguiAle@users.noreply.github.com>
2019-10-03 14:49:25 -0400

    Update sketch_sep30a.ino

--- Codigos Hugo/sketch_sep30a/sketch_sep30a.ino
+++ Codigos Hugo/sketch_sep30a/sketch_sep30a.ino
@@ -1,10 +1,11 @@
 int  motor=16;
+int  motor2=05;
 int boton=15;
 
 void setup() {
 pinMode(A0,INPUT);
 pinMode(motor,OUTPUT);
-
+pinMode(motor2,OUTPUT);
 Serial.begin(9600);
 
 }
@@ -12,10 +13,18 @@ Serial.begin(9600);
 void loop() {
 
   Serial.println(analogRead(A0));
- 
-  int t =map(analogRead(A0),6,1024,1,500);
+  int t2=map(analogRead(A0),1,500,30,500); 
+  int t =map(analogRead(A0),550,1024,30,500);
+  
+  delay(t);
   digitalWrite(motor,HIGH);
   delay(t);
   digitalWrite(motor,LOW);
-  delay(t);
+  
+  delay(t2);
+  digitalWrite(motor2,HIGH);
+  delay(t2);   
+  digitalWrite(motor2,LOW);
+  
+
 }
